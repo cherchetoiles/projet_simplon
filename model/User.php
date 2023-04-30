7112a990b65036b8bd576968496d7eb6f21294cb
Graphyk <gueretlouis13@gmail.com>
2023-04-30 12:41:26 +0200

    classe user repo et user sans method

---
+++ model/User.php
@@ -0,0 +1,15 @@
+<?php
+class User
+{
+    public $user_id;
+    public $user_name;
+    public $user_surname;
+    public $user_email;
+    public $user_password;
+    public $user_statut;
+    public $user_token;
+    public $user_avatar;
+    public $role_nom;
+    public $speciality_name;
+}
+?>
---
+++ repository/User_repo.php
@@ -0,0 +1,8 @@
+<?php
+class UserRepository extends Connect_bdd{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+?>
