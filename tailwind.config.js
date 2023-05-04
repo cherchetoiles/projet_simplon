e88b8f2547cae2bad117d62c477d7d864e19e352
Graphyk <gueretlouis13@gmail.com>
2023-05-04 16:14:01 +0200

    footer FULL FRONT #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./view/*.{html,js,php}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors : {
+        'red':'#f01e29',
+        'stroke':'#D9D9D9',
+        'blue':'#016484',
+        'gray-bg':'#EAEAEA',
+        'gray-dark':'#858585',
+        'gray':'#B7B7B7',
+
+      },
+      borderRadius: {
+        '4xl':'40px',
+     },
+      fontFamily:{
+        'body':'"Poppins", sans-serif',
+        'sans':'"Montserrat", sans-serif',
+      },
+      plugins: [],
+    }
+  }
 }
--- view/index.php
+++ view/index.php
@@ -5,11 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="dist/output.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
-<body class="h-screen">
-    <section class="bg-red-700 h-screen">
-        <p class="text-green-600">test</p>
-    </section>
+<body>
+    <?php
+    include("view/footer.php");
+    ?>
 </body>
 </html>
