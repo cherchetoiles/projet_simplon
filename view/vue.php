888b45d2fd296891a942953ab1690259420d1587
khaled97dev <khaled.service.civique.com>
2023-05-25 14:58:38 +0200

    VUEresponsive

--- view/vue.php
+++ view/vue.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Primier Vue</title>
+  <title>Premier Vue</title>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
   <link href="dist/output.css" rel="stylesheet">
   <style>
@@ -22,44 +22,40 @@
       text-align: center;
       border-radius: 10px;
     }
-    .stroky {
-        stroke: red;
-    }
+   
   </style>
 </head>
 
 <body class="font-body">
   <div class="container">
     <div class="content">
-      <h2 class="text-6xl font-bold">Elève de simplon ?</h2>
-      <p class="pt-3">Faites partie de nos <span class="font-bold">créateurs</span> et battez les records de vues pour partager ton savoir et obtenir des récompenses !</p>
+      <h2 class="text-4xl md:text-6xl md:pt-[30px] font-bold">Elève de simplon ?</h2>
+      <p class="pt-3 text-sm md:text-base">Faites partie de nos <span class="font-bold">créateurs</span> et battez les records de vues pour partager ton savoir et obtenir des récompenses !</p>
       <form action="">
-        <div class="text-2xl  text-left flex ">
-            <div class=" w-1/2 p-8">
+        <div class="text-2xl text-sm md:text-base text-left flex flex-col md:flex-row">
+            <div class="w-full md:w-1/2 p-8">
                 <div class="pb-9">
-                    <p class="font-bold">Dans quel formation es-tu ?</p>
-                    <select id="dropdown1" name="dropdown1" class="w-96 ml-[120px] mt-3 text-lg p-2 block border border-indigo-600 border-slate-300 rounded-md">
-                    <option class="" value="1">Développeur Web et Web mobile</option>
+                    <p class="font-bold text-sm md:text-base">Dans quel formation es-tu ?</p>
+                    <select id="dropdown1" name="dropdown1" class="w-full md:w-96 ml-0 md:ml-[120px] mt-3 text-base md:text-lg p-2 block border border-indigo-600 border-slate-300 rounded-md">
+                      <option class="" value="1">Développeur Web et Web mobile</option>
                     </select>
                 </div>
                 <div>
-                    <p class="font-bold">Souhaites-tu contribuer à nos cours ?</p>
-                    <select id="dropdown2" name="dropdown2" class="w-96 ml-[120px] mt-3 text-lg p-2 block border border-indigo-600 border-slate-300  rounded-md">
-                    <option value="1">Je souhaite posté des cours</option>
+                    <p class="font-bold text-sm md:text-base">Souhaites-tu contribuer à nos cours ?</p>
+                    <select id="dropdown2" name="dropdown2" class="w-full md:w-96 ml-0 md:ml-[120px] mt-3 text-base md:text-lg p-2 block border border-indigo-600 border-slate-300 rounded-md">
+                      <option value="1">Je souhaite poster des cours</option>
                     </select>
                 </div>
             </div>
-            <div class="w-1/2">
+            <div class="w-full md:w-1/2 flex justify-center md:justify-end">
                  <img src="assets/vuu.png" alt="Photo" class="max-w-full h-auto">
             </div>
         </div>
-        <div class="w-full flex  justify-center">
-                <button type="submit" class=" p-3 text-white bg-red border rounded-md">Soumettre mon dossier</button>
+        <div class="w-full flex justify-center">
+                <button type="submit" class="p-3 text-white bg-red border rounded-md">Soumettre mon dossier</button>
         </div>
-             
       </form>  
     </div>
   </div>
 </body>
-
 </html>
