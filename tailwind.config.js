8e3a4369676ff25b426bb479e3669a1aa0587790
MoulayZiad <ziadelidrissi1@gmail.com>
2023-05-26 08:38:14 +0200

    fix: navbar redone first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./view/*.{html,js,php}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors : {
+        'red':'#f01e29',
+        'stroke':'#D9D9D9',
+        'blue':'#016484',
+        'gray-bg':'#EAEAEA',
+        'gray-dark':'#858585',
+        'gray':'#B7B7B7',
+        'hovered':"#B4131B"
+      },
+      borderRadius: {
+        '4xl':'40px',
+     },
+      fontFamily:{
+        'body':'"Poppins", sans-serif',
+        'sans':'"Montserrat", sans-serif',
+      },
+      plugins: [],
+      boxShadow: {
+        'lg': '20px 25px 20px -20px rgba(0, 0, 0, 0.3)',
+      }
+    }
+  }
 }
--- view/navbar.php
+++ view/navbar.php
@@ -2,74 +2,10 @@
   <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
   <div class="drawer-content flex flex-col">
     <!-- Navbar -->
-    <div class="border-b">
-      <div class="navbar py-0 flex items-center justify-between h-full md:max-w-2xl md:mx-auto lg:max-w-4xl xl:max-w-6xl">
-        <div class="dropdown dropdown-right sm:hidden">
-          <label tabindex="0" class="btn btn-ghost btn-ghost">
-            <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g clip-path="url(#clip0_11_796)">
-              <path d="M16.1875 16.1875L19.25 19.25" stroke="#F01E29" stroke-width="2" stroke-linecap="round"/>
-              <path d="M5.90625 2.86206C7.12891 2.15479 8.54843 1.75 10.0625 1.75C14.6534 1.75 18.375 5.47163 18.375 10.0625C18.375 14.6534 14.6534 18.375 10.0625 18.375C5.47163 18.375 1.75 14.6534 1.75 10.0625C1.75 8.54843 2.15479 7.12891 2.86206 5.90625" stroke="#F01E29" stroke-width="2" stroke-linecap="round"/>
-              </g>
-              <defs>
-              <clipPath id="clip0_11_796">
-              <rect width="21" height="21" fill="white"/>
-              </clipPath>
-              </defs>
-            </svg>
-          </label>
-          <input type="search" class="dropdown-content p-2 rounded-full w-40 border-2" placeholder="Rechercher...">
-        </div>
+    <div class="w-full navbar max-h-[4rem] justify-between border-b">
 
-        <div class="px-2 mx-2 cursor-pointer">
-          <img src="assets/logo.png" class="w-44 sm:w-52 md:w-60" alt="">
-        </div>
-
-        <div class="hidden sm:block sm:flex-grow lg:hidden">
-
-        </div>
-
-        <div class="hidden lg:flex lg:flex-row lg:items-center lg:gap-3 lg:h-full">
-          <div class="dropdown h-full flex items-center cursor-pointer hover:border-b-4 hover:border-b-[#F01E29]">
-            <label class="flex flex-row items-center cursor-pointer text-xl" tabindex="0">
-              Cours
-              <svg class="mx-2 mt-1" width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#F01E29" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-            </label>
-            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 mt-48">
-              <li><a>Item 1</a></li>
-              <li><a>Item 2</a></li>
-            </ul>
-          </div>
-          <div class="dropdown h-full flex items-center cursor-pointer hover:border-b-4 hover:border-b-[#F01E29]">
-            <label class="flex flex-row items-center cursor-pointer text-xl" tabindex="0">
-              Formations
-              <svg class="mx-2 mt-1" width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#F01E29" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-            </label>
-            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 mt-48">
-              <li><a>Item 1</a></li>
-              <li><a>Item 2</a></li>
-            </ul>
-          </div>
-          <div class="dropdown h-full flex items-center cursor-pointer hover:border-b-4 hover:border-b-[#F01E29]">
-            <label class="flex flex-row items-center cursor-pointer text-xl" tabindex="0">
-              Forum
-              <svg class="mx-2 mt-1" width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#F01E29" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-              </svg>
-            </label>
-            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 mt-48">
-              <li><a>Item 1</a></li>
-              <li><a>Item 2</a></li>
-            </ul>
-          </div>
-        </div>
-
-        <div class="hidden p-2 rounded-full w-40 border-2 sm:flex sm:flex-row">
-          <input type="search" class="w-full mx-2 focus:outline-0 md:" placeholder="Rechercher...">
+      <div class="flex-none lg:hidden">
+        <label for="" class="btn btn-square btn-ghost">
           <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clip-path="url(#clip0_11_796)">
             <path d="M16.1875 16.1875L19.25 19.25" stroke="#F01E29" stroke-width="2" stroke-linecap="round"/>
@@ -78,125 +14,112 @@
             <defs>
             <clipPath id="clip0_11_796">
             <rect width="21" height="21" fill="white"/>
-            </clipPath> 
+            </clipPath>
             </defs>
           </svg>
-        </div>
-
-        <div class="lg:hidden">
-          <label for="my-drawer-3" class="btn btn-square btn-ghost">
-            <svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M21 18L1 18" stroke="black" stroke-width="2" stroke-linecap="round"/>
-              <path d="M21 9.5L1 9.5" stroke="black" stroke-width="2" stroke-linecap="round"/>
-              <path d="M21 1L1 1" stroke="black" stroke-width="2" stroke-linecap="round"/>
-            </svg>
-          </label>
-        </div>
-
-        <div class="hidden placeholder dropdown lg:block cursor-pointer">
-          <div tabindex="0" class="bg-gray-200 text-neutral-content rounded-full w-16 h-10 flex justify-center items-center gap-1">
-            <span class="bg-[#016484] rounded-full h-6 w-6 text-center">Z</span>
-            <svg class="mt-1" width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#016484" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            </svg>
-          </div>
-          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Item 1</a></li>
-            <li><a>Item 2</a></li>
-          </ul>
-        </div> 
-
-      </div>
-    </div>
-    
-  </div> 
-
-  <!-- DRAWER -->
-  <div class="drawer-side">
-    <label for="my-drawer-3" class="drawer-overlay lg:hidden"></label> 
-    <ul class="menu p-4 w-52 bg-base-100 sm:w-80 lg:hidden">
-      <div class="flex justify-between lg:hidden border-b-2">
-        <div class="dropdown dropdown-right">
-          <label tabindex="0" class="btn btn-ghost px-0">
-            <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <g clip-path="url(#clip0_11_796)">
-              <path d="M16.1875 16.1875L19.25 19.25" stroke="#F01E29" stroke-width="2" stroke-linecap="round"/>
-              <path d="M5.90625 2.86206C7.12891 2.15479 8.54843 1.75 10.0625 1.75C14.6534 1.75 18.375 5.47163 18.375 10.0625C18.375 14.6534 14.6534 18.375 10.0625 18.375C5.47163 18.375 1.75 14.6534 1.75 10.0625C1.75 8.54843 2.15479 7.12891 2.86206 5.90625" stroke="#F01E29" stroke-width="2" stroke-linecap="round"/>
-              </g>
-              <defs>
-              <clipPath id="clip0_11_796">
-              <rect width="21" height="21" fill="white"/>
-              </clipPath>
-              </defs>
-            </svg>
-          </label>
-          <input type="search" class="dropdown-content p-2 rounded-full w-40 border-2" placeholder="Rechercher...">
-        </div>
-        <label for="my-drawer-3" class="btn btn-ghost px-0">
-          <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M17.9999 1.00007L1 18M0.999928 1L17.9999 17.9999" stroke="#393536" stroke-width="1.5" stroke-linecap="round"/>
-          </svg>
         </label>
-      </div>
-      <!-- Sidebar content here -->
-      <div class="collapse border-b-2">
-        <input type="checkbox" />
-        <div class="flex flex-row items-center collapse-title text-xl font-medium px-0">
-          <svg class="mx-2" width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#F01E29" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
-          Python
-        </div>
-        <ul class="ml-8 collapse-content">
-          <li>Sous-catégorie</li>
-          <li>Sous-catégorie</li>
-          <li>Sous-catégorie</li>
-        </ul>
-      </div>
-      <div class="collapse border-b-2">
-        <input type="checkbox" />
-        <div class="flex flex-row items-center collapse-title text-xl font-medium px-0">
-          <svg class="mx-2" width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#F01E29" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
-          Java
-        </div>
-        <ul class="ml-8 collapse-content">
-          <li>Sous-catégorie</li>
-          <li>Sous-catégorie</li>
-          <li>Sous-catégorie</li>
-        </ul>
-      </div>
-      <div class="collapse border-b-2">
-        <input type="checkbox" />
-        <div class="flex flex-row items-center collapse-title text-xl font-medium px-0">
-          <svg class="mx-2" width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#F01E29" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
-          HTML
-        </div>
-        <ul class="ml-8 collapse-content">
-          <li>Sous-catégorie</li>
-          <li>Sous-catégorie</li>
-          <li>Sous-catégorie</li>
-        </ul>
-      </div>
-      <div class="collapse border-b-2">
-        <input type="checkbox" />
-        <div class="flex flex-row items-center collapse-title text-xl font-medium px-0">
-          <svg class="mx-2" width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#016484" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-          </svg>
-          Mon compte
-        </div>
-        <ul class="ml-8 collapse-content">
-          <li>Sous-catégorie</li>
-          <li>Sous-catégorie</li>
-          <li>Sous-catégorie</li>
-        </ul>
+        
       </div>
       
-    </ul>
-    
-  </div>
+      <div class="  px-2 mx-2">
+        <img class="w-44 sm:w-52 md:w-60" src="assets/logo.png" alt="">
+      </div>
+
+      <div class="flex-none lg:hidden">
+        <label for="my-drawer-3" class="btn btn-square btn-ghost">
+          <svg width="22" height="19" viewBox="0 0 22 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M21 18L1 18" stroke="black" stroke-width="2" stroke-linecap="round"/>
+            <path d="M21 9.5L1 9.5" stroke="black" stroke-width="2" stroke-linecap="round"/>
+            <path d="M21 1L1 1" stroke="black" stroke-width="2" stroke-linecap="round"/>
+          </svg>
+        </label>
+      </div> 
+
+    </div>
+    <!-- Page content here -->
+    Content
+
+  <!--NAV IN FOOTER -->
+    </div> 
+    <div class="drawer-side">
+      <label for="my-drawer-3" class="drawer-overlay"></label> 
+      <ul class="menu bg-base-100 w-3/4">
+        <!-- Sidebar content here -->
+        <div class="flex items-center h-16 px-8 py-4 bg-gray">
+          <label for="my-drawer-3">
+            <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M17.9999 1.00007L1 18M0.999928 1L17.9999 17.9999" stroke="#393536" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
+          </label>
+        </div>
+
+        <div class="px-4 py-2 flex flex-row items-center gap-2 font-extrabold border-b border-b-gray">
+          <svg width="20" height="17" viewBox="0 0 20 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5.01031 3.82959L3.31384 5.35641C1.77128 6.74471 1 7.43886 1 8.32959C1 9.22031 1.77128 9.91447 3.31384 11.3028L5.01031 12.8296" stroke="#B7B7B7" stroke-linecap="round"/>
+            <path d="M11.997 1L11.0036 4.70743M9.85345 9L8.02344 15.8297" stroke="#B7B7B7" stroke-linecap="round"/>
+            <path d="M15.0103 3.82959L16.7067 5.35641C18.2493 6.74471 19.0206 7.43886 19.0206 8.32959C19.0206 9.22031 18.2493 9.91447 16.7067 11.3028L15.0103 12.8296" stroke="#B7B7B7" stroke-linecap="round"/>
+          </svg>
+          <span class="text-gray font-body">Code</span>
+        </div>
+
+        <div class="collapse border-b border-b-gray">
+          <input type="checkbox" />
+          <div class="collapse-title text-xl font-bold font-body px-8 flex flex-row items-center gap-2">
+            Python
+            <div class="">
+              <svg width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#F01E29" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg> 
+            </div>
+          </div>
+          <div class="collapse-content px-8">
+            <p>hello</p>
+          </div>
+        </div>
+        <div class="collapse border-b border-b-gray">
+          <input type="checkbox" />
+          <div class="collapse-title text-xl font-bold font-body px-8 flex flex-row items-center gap-2">
+            Python
+            <div class="">
+              <svg width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#F01E29" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg> 
+            </div>
+          </div>
+          <div class="collapse-content px-8">
+            <p>hello</p>
+          </div>
+        </div>
+        <div class="collapse border-b border-b-gray">
+          <input type="checkbox" />
+          <div class="collapse-title text-xl font-bold font-body px-8 flex flex-row items-center gap-2">
+            Python
+            <div class="">
+              <svg width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#F01E29" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg> 
+            </div>
+          </div>
+          <div class="collapse-content px-8">
+            <p>hello</p>
+          </div>
+        </div>
+        <div class="collapse border-b border-b-gray">
+          <input type="checkbox" />
+          <div class="collapse-title text-xl font-bold font-body px-8 flex flex-row items-center gap-2">
+            Mon compte
+            <div class="">
+              <svg width="15" height="10" viewBox="0 0 15 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M13.8327 1.83234L7.50032 8.83234L1.16797 1.83234" stroke="#016484" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg> 
+            </div>
+          </div>
+          <div class="collapse-content px-8">
+            <p>hello</p>
+          </div>
+        </div>
+
+      </ul>
+    </div>
+  <!--NAV IN FOOTER -->
 </div>
