382e0652c4e0210fcbc16823467689f21bdd46d0
khaled97dev <khaled.service.civique.com>
2023-05-26 16:12:14 +0200

    category treat

--- model/Category.php
+++ model/Category.php
@@ -5,23 +5,39 @@ class Category
     private string $category_name;
     private string $category_logo;
     private string $category_description;
-    private string $category_video;
     private array $category_views;
     private array $lessonsFromCategory;
     private array $categoriesNeeded;
     private int $theme_id;
 
-    public function createCategoryFromRequest($category_id,$category_name,$category_logo,$category_description,$theme_id,$category_video){
+    public function createCategoryFromRequest($category_id,$category_name,$category_logo,$category_description,$theme_id){
         $this->category_id=$category_id;
         $this->category_name=$category_name;
         $this->category_logo=$category_logo;
         $this->category_description=$category_description;
-        $this->category_video=$category_video;
         $this->theme_id=$theme_id;
     }
 
     public function getCategoryId(){
         return $this->category_id;
+    }
+    public function getCategoryName(){
+        return $this->category_name;
+    }
+    public function getCategoryLogo(){
+        return $this->category_logo;
+    }
+    public function getCategoryDescription(){
+        return $this->category_description;
+    }
+    public function getThemeId(){
+        return $this->theme_id;
+    }
+    public function createCategoryToInsert($category_name,$category_logo,$category_description,$theme_id){
+        $this->category_name=$category_name;
+        $this->category_logo=$category_logo;
+        $this->category_description=$category_description;
+        $this->theme_id;
     }
 }
 ?>
