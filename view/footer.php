600a7c1058918cc6f945e42a9844684f78855df0
MoulayZiad <ziadelidrissi1@gmail.com>
2023-06-06 16:22:20 +0200

    feat: footer margins

--- view/footer.php
+++ view/footer.php
@@ -1,52 +1,58 @@
-<div class="w-full mx-auto border-t-2 border-solid border-gray-bg">
-    <div class="flex flex-col items-center lg:flex-row lg:justify-between lg:my-5 lg:mx-5 lg:gap-8">
-        <div class="text-center lg:w-72">
-            <img src="assets/svg/logo.svg">
-            <span class="text-red tracking-[0.35em] font-semibold text-sm lg:text-xs">APPRENDRE EN LIGNE</span>
-        </div>
-        <span class="mx-5 mt-10 font-light leading-7 text-center font-body lg:mt-0 lg:w-7/12 text-gray-dark lg:mx-0">
-            En tant qu'apprenants, nous avons mis en place ce site afin d'aider nos futurs successeurs à acquérir des connaissances de manière simple et intuitive !
-        </span>
-        <div class="flex flex-col mt-10 lg:mt-0">
-            <span class="font-sans text-blue">Code</span>
-            <div class="grid grid-cols-2 text-gray-dark gap-x-12 font-body">
-                <span>Python</span>
-                <span>HTML</span>
-                <span>JS</span>
-                <span>PHP</span>
-                <span>CSS</span>
-                <span>Java</span>
+<!-- FOOTER -->
+<div class="border-t-2 border-solid border-gray-bg">
+    <div class="w-4/5 mx-auto px-8">
+        <div class="flex flex-col items-center lg:flex-row lg:justify-between lg:my-5 lg:mx-5 lg:gap-8">
+            <div class="text-center lg:w-72">
+                <img src="assets/svg/logo.svg">
+                <span class="text-red tracking-[0.35em] font-semibold text-sm lg:text-xs">APPRENDRE EN LIGNE</span>
+            </div>
+            <span class="mx-5 mt-10 font-light leading-7 text-center font-body lg:mt-0 lg:w-7/12 text-gray-dark lg:mx-0">
+                En tant qu'apprenants, nous avons mis en place ce site afin d'aider nos futurs successeurs à acquérir des connaissances de manière simple et intuitive !
+            </span>
+            <div class="flex flex-col mt-10 lg:mt-0">
+                <span class="font-sans text-blue">Code</span>
+                <div class="grid grid-cols-2 text-gray-dark gap-x-12 font-body">
+                    <span>Python</span>
+                    <span>HTML</span>
+                    <span>JS</span>
+                    <span>PHP</span>
+                    <span>CSS</span>
+                    <span>Java</span>
+                </div>
             </div>
         </div>
     </div>
-    <div class="relative flex flex-col items-center gap-2 px-10 pt-10 mt-4 font-light border-t-2 border-solid lg:pt-4 border-gray-bg lg:flex-row justify-evenly lg:gap-0 text-gray-dark font-body">
-        <div class="flex flex-col items-center lg:flex-row">
-            <span>
-                ©Copyright Simplon.co
-            </span>
-            <span class="hidden lg:inline-block">-</span>
-            <span class="font-bold">
-                K-ZEL CODE
-            </span>
-            <span class="hidden lg:inline-block">-</span>
-            <span>
-                Politique de confidentialité
-            </span>
-            <span class="hidden lg:inline-block">-</span>
-            <span>
-                Utilisation des cookies
-            </span>
-            <span class="hidden lg:inline-block">-</span>
-            <span>
-                Mentions légales
-            </span>
-        </div>
-        <div class="lg:absolute right-5 bottom-1.5 flex gap-2 my-2 lg:my-0">
-            <a href="https://www.linkedin.com/company/simplon-charleville/" target="blank"><img src="assets/svg/in.svg"></a>
-            <a href="https://www.facebook.com/ecoledescodeurs" target="_blank"><img src="assets/svg/fb.svg"></a>
+    <div class="border-t-2 border-solid border-gray-bg">
+        <div class="w-4/5 mx-auto relative flex flex-col items-center gap-2 px-10 pt-10 mt-4 font-light  lg:pt-4 border-gray-bg lg:flex-row justify-evenly lg:gap-0 text-gray-dark font-body">
+            <div class="flex flex-col items-center lg:flex-row">
+                <span>
+                    ©Copyright Simplon.co
+                </span>
+                <span class="hidden lg:inline-block">-</span>
+                <span class="font-bold">
+                    K-ZEL CODE
+                </span>
+                <span class="hidden lg:inline-block">-</span>
+                <span>
+                    Politique de confidentialité
+                </span>
+                <span class="hidden lg:inline-block">-</span>
+                <span>
+                    Utilisation des cookies
+                </span>
+                <span class="hidden lg:inline-block">-</span>
+                <span>
+                    Mentions légales
+                </span>
+            </div>
+            <div class="lg:absolute right-5 bottom-1.5 flex gap-2 my-2 lg:my-0">
+                <a href="https://www.linkedin.com/company/simplon-charleville/" target="blank"><img src="assets/svg/in.svg"></a>
+                <a href="https://www.facebook.com/ecoledescodeurs" target="_blank"><img src="assets/svg/fb.svg"></a>
+            </div>
         </div>
     </div>
 </div>
+<!-- FOOTER -->
 
 <!--NAV IN FOOTER -->
 </div> 
