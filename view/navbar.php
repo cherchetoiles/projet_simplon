28e25dcfdcda5ef944372b97a046631f196e1bf5
cherchetoiles <elise.cherchetoiles@gmail.com>
2023-06-07 10:50:28 +0200

    profil problem

--- view/navbar.php
+++ view/navbar.php
@@ -104,7 +104,7 @@ $categories=$repo -> getAllCategories();
           </div>
 
 
-          <a class="flex-row hidden gap-2 p-2 cursor-pointer lg:flex bg-gray/20 rounded-4xl" href="?action=profil&userId=<?=$_SESSION['user']->getUserId()?>">
+          <a class="flex-row hidden gap-2 p-2 cursor-pointer lg:flex bg-gray/20 rounded-4xl" href="?action=profil">
             <img src="assets/img/user_avatar/<?php echo $_SESSION["user"]->getUserAvatar();?>" class="rounded-full">
             <span class="hidden text-blue xl:block">Profil</span>
           </a>
